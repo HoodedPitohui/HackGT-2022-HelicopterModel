5cfcf51e1978646a839d86d77970ca7dc270e793
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-10-21 23:42:35 -0400

    initial-data-pushing

--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -1,3 +1,26 @@
 import matplotlib
 import scipy
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+save_path = os.path.join("data")
+
+def parse_data(given_path):
+    file_name = 'metars_cache_1.csv'
+    metars_path = os.path.join(given_path, file_name)
+    metars_data = pd.read_csv(metars_path)
+    return metars_data
+
+def main():
+    given_data = parse_data(save_path)
+    print('hello')
+    plt.scatter(given_data['latitude'], given_data['longitude'])
+    plt.show()
+    plt.scatter(given_data['latitude'], given_data['altim_in_hg'])
+    print('test')
+
+
+if __name__ == "__main__":
+    main()
